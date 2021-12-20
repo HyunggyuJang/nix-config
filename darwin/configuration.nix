0d7d56705acacf90e931b2256ec2fe673a2b3d04
Hyunggyu Jang <murasakipurplez5@gmail.com>
2021-12-21 08:28:57 +0900

    vterm setting was necessary

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -2369,6 +2369,36 @@ yabai -m rule --add app="^zoom$" space=4
           echo -e "\e[1;31merror: Homebrew is not installed, skipping...\e[0m" >&2
         fi
     '';
+                initExtra = ''
+        PROMPT=' %{$fg_bold[blue]%}$(get_pwd)%{$reset_color%} ''${prompt_suffix}'
+        local prompt_suffix="%(?:%{$fg_bold[green]%}❯ :%{$fg_bold[red]%}❯%{$reset_color%} "
+        function get_pwd(){
+            git_root=$PWD
+            while [[ $git_root != / && ! -e $git_root/.git ]]; do
+                git_root=$git_root:h
+            done
+            if [[ $git_root = / ]]; then
+                unset git_root
+                prompt_short_dir=%~
+            else
+                parent=''${git_root%\/*}
+                prompt_short_dir=''${PWD#$parent/}
+            fi
+            echo $prompt_short_dir
+                                    }
+        vterm_printf(){
+            if [ -n "$TMUX" ]; then
+                # Tell tmux to pass the escape sequences through
+                # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+                printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+            elif [ "''${TERM%%-*}" = "screen" ]; then
+                # GNU screen (screen, screen-256color, screen-256color-bce)
+                printf "\eP\e]%s\007\e\\" "$1"
+            else
+                printf "\e]%s\e\\" "$1"
+            fi
+                                                                          }
+        '';
             };
 
             programs.fzf.enable = true;
