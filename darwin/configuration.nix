afd91cb5820c2933f52c1838547f158a694c6d67
Hyunggyu Jang <murasakipurplez5@gmail.com>
2022-02-23 16:12:40 +0900

    Revert "Install virtualbox for isucon"
    This reverts commit 33839b4cb173b511e5649f8926d943288ba41fb9.
    
    VirtualBox doesn't support M1

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -4504,7 +4504,6 @@ ctrl + shift + cmd - e : skhd -k "cmd - a"; doom everywhere
                 "font-fira-mono"
                 "discord"
                 "vagrant"
-                "virtualbox"
             ] ++ (if localconfig.hostname == "work" then [
                 # IBM fonts
                 "font-ibm-plex"
