7da21feebf07ffbe0264f56c4db73b3c45702962
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-01-28 14:35:09 +0900

    Disable default keyboard due to KkukKkuk

--- overlays/rust/default.nix
+++ overlays/rust/default.nix
@@ -3,16 +3,16 @@ with prev;
 {
   yaskkserv2 = rustPlatform.buildRustPackage rec {
     pname = "yaskkserv2";
-    version = "0.1.1";
+    version = "0.1.5";
     src = fetchFromGitHub {
       owner = "wachikun";
       repo = pname;
-      rev = "982845af2dd8eca6675dc3c9eded24570a205142";
-      sha256 = "13nadqm977rc6z7zka7ymdrh717r62w3s6k15dll4way6djvp3y6";
+      rev = "48ede49d4a848de5e1787b270d61f03ed0176dd3";
+      sha256 = "sha256-zXwyiI+oJv5ZUrl9OVWZ8VUFEaAf+kajRCkXRVihyFc=";
     };
     # target = "x86_64-apple-darwin";
 
-    cargoSha256 = "sha256-IgwnJqm7HcfxFnoGK1kYTNgDnPhYZpztXEnfNwcEIAM=";
+    cargoSha256 = "sha256-r5PWeRvUyzuV26Eq4FWW67WCXBqQmpT8gBJujCys4k4=";
 
     buildInputs = [ darwin.apple_sdk.frameworks.Security ];
 
