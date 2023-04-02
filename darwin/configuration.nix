981014746a93f6dcab154a250c455a798e9aec54
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-02 10:15:13 +0900

    NVM setting if not there

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -2352,13 +2352,13 @@ yabai -m rule --add app="^zoom$" space=4
             fi
                                                                                 }
 
-        if [[ $INSIDE_EMACS != vterm && $TERM_PROGRAM != vscode ]]; then
+        if declare -F nvm; then
             export NVM_DIR="$HOME/.nvm"
             [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
             [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
             # opam
-            [[ ! -r /Users/hyunggyujang/.opam/opam-init/init.zsh ]] || source /Users/hyunggyujang/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+            # [[ ! -r /Users/hyunggyujang/.opam/opam-init/init.zsh ]] || source /Users/hyunggyujang/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
         fi
         '';
       };
