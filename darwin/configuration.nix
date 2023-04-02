8b53a6485c6005f60737a9d5fab19799c6ae2c4d
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-02 14:17:56 +0900

    Autoload .nvmrc

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -2357,8 +2357,32 @@ yabai -m rule --add app="^zoom$" space=4
             [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
             [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
+
             # opam
             # [[ ! -r /Users/hyunggyujang/.opam/opam-init/init.zsh ]] || source /Users/hyunggyujang/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+        fi
+
+        if ! declare -F load-nvmrc; then
+            autoload -U add-zsh-hook
+            load-nvmrc() {
+              local node_version="$(nvm version)"
+              local nvmrc_path="$(nvm_find_nvmrc)"
+
+              if [ -n "$nvmrc_path" ]; then
+                local nvmrc_node_version=$(nvm version "$(cat "$nvmrc_path")")
+
+                if [ "$nvmrc_node_version" = "N/A" ]; then
+                  nvm install
+                elif [ "$nvmrc_node_version" != "$node_version" ]; then
+                  nvm use
+                fi
+              elif [ "$node_version" != "$(nvm version default)" ]; then
+                echo "Reverting to nvm default version"
+                nvm use default
+              fi
+            }
+            add-zsh-hook chpwd load-nvmrc
+            load-nvmrc
         fi
         '';
       };
