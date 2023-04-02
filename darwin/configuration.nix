efd3c559a6b30515bd6af1a419732b9faec01043
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-02 14:14:06 +0900

    Set nvm if not set

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -2352,7 +2352,7 @@ yabai -m rule --add app="^zoom$" space=4
             fi
                                                                                 }
 
-        if declare -F nvm; then
+        if ! declare -F nvm; then
             export NVM_DIR="$HOME/.nvm"
             [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
             [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
