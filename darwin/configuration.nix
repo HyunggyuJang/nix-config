c214d1652479548edc361eb4420b2372b77f5e62
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-03-24 23:35:42 +0900

    UPdate

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -67,7 +67,7 @@ in with lib;
     ];
 
     home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
+    home-manager.useUserPackages = false;
     home-manager.users = let userconfig = { config, ...}: rec {
       home.stateVersion = "24.05";
       home.file = {
@@ -2276,72 +2276,73 @@ kitty --listen-on unix:/tmp/mykitty --single-instance --directory "$DIR"
 '';
         };
       };
-      programs.zsh = {
-        enable = true;
-        autosuggestion.enable = true;
-        enableCompletion = false; # See https://github.com/NixOS/nix/issues/5445
-        defaultKeymap = "emacs";
-        sessionVariables = { RPROMPT = ""; };
-        shellAliases =  {
-          dbuild = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color make && cd -";
-          dswitch = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color caffeinate -i make switch && cd -";
-          drb = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color make rollback && cd -";
-        };
+      programs = {
+        zsh = {
+          enable = true;
+          autosuggestion.enable = true;
+          enableCompletion = false; # See https://github.com/NixOS/nix/issues/5445
+          defaultKeymap = "emacs";
+          sessionVariables = { RPROMPT = ""; };
+          shellAliases =  {
+            dbuild = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color make && cd -";
+            dswitch = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color caffeinate -i make switch && cd -";
+            drb = "cd ${hgj_darwin_home} && HOSTNAME=${localconfig.hostname} TERM=xterm-256color make rollback && cd -";
+          };
 
-        oh-my-zsh.enable = true;
+          oh-my-zsh.enable = true;
 
-        plugins = [
-          {
-            name = "autopair";
-            file = "autopair.zsh";
-            src = pkgs.fetchFromGitHub {
-              owner = "hlissner";
-              repo = "zsh-autopair";
-              rev = "9d003fc02dbaa6db06e6b12e8c271398478e0b5d";
-              sha256 = "sha256-hwZDbVo50kObLQxCa/wOZImjlH4ZaUI5W5eWs/2RnWg=";
-            };
-          }
-          {
-            name = "fast-syntax-highlighting";
-            file = "fast-syntax-highlighting.plugin.zsh";
-            src = pkgs.fetchFromGitHub {
-              owner = "zdharma-continuum";
-              repo = "fast-syntax-highlighting";
-              rev = "585c089968caa1c904cbe926ff04a1be9e3d8f42";
-              sha256 = "sha256-x+4C2u03RueNo6/ZXsueqmYoPIpDHnKAZXP5IiKsidE=";
-            };
-          }
-          {
-            name = "z";
-            file = "zsh-z.plugin.zsh";
-            src = pkgs.fetchFromGitHub {
-              owner = "agkozak";
-              repo = "zsh-z";
-              rev = "b30bc6050e77abe30ce36761d18ed696e5410f16";
-              sha256 = "sha256-TSX6KooWYGf1NDlD4A3o6CmSsyy1JL7bPeKsuCOuUhY=";
-            };
-          }
-          rec {
-            name = "system-wide-clipboard";
-            file = "system-wide-clipboard.zsh";
-            src = pkgs.stdenv.mkDerivation rec {
-              name    = "system-wide-clipboard";
-              src = pkgs.fetchurl {
-                name = "system-wide-clipboard.zsh";
-                url = "https://gist.githubusercontent.com/HyunggyuJang/850b22128515b257ff3da73b589d7d3b/raw/3660504d2874a46a048b291a8ceabe8af9778294/system-wide-clipboard.zsh";
-                sha256 = "sha256-fmLcHhD2Cb45OEmIQi8mp9Q1uid1Osy9/kFxelHp70Y=";
+          plugins = [
+            {
+              name = "autopair";
+              file = "autopair.zsh";
+              src = pkgs.fetchFromGitHub {
+                owner = "hlissner";
+                repo = "zsh-autopair";
+                rev = "9d003fc02dbaa6db06e6b12e8c271398478e0b5d";
+                sha256 = "sha256-hwZDbVo50kObLQxCa/wOZImjlH4ZaUI5W5eWs/2RnWg=";
               };
+            }
+            {
+              name = "fast-syntax-highlighting";
+              file = "fast-syntax-highlighting.plugin.zsh";
+              src = pkgs.fetchFromGitHub {
+                owner = "zdharma-continuum";
+                repo = "fast-syntax-highlighting";
+                rev = "585c089968caa1c904cbe926ff04a1be9e3d8f42";
+                sha256 = "sha256-x+4C2u03RueNo6/ZXsueqmYoPIpDHnKAZXP5IiKsidE=";
+              };
+            }
+            {
+              name = "z";
+              file = "zsh-z.plugin.zsh";
+              src = pkgs.fetchFromGitHub {
+                owner = "agkozak";
+                repo = "zsh-z";
+                rev = "b30bc6050e77abe30ce36761d18ed696e5410f16";
+                sha256 = "sha256-TSX6KooWYGf1NDlD4A3o6CmSsyy1JL7bPeKsuCOuUhY=";
+              };
+            }
+            rec {
+              name = "system-wide-clipboard";
+              file = "system-wide-clipboard.zsh";
+              src = pkgs.stdenv.mkDerivation rec {
+                name    = "system-wide-clipboard";
+                src = pkgs.fetchurl {
+                  name = "system-wide-clipboard.zsh";
+                  url = "https://gist.githubusercontent.com/HyunggyuJang/850b22128515b257ff3da73b589d7d3b/raw/3660504d2874a46a048b291a8ceabe8af9778294/system-wide-clipboard.zsh";
+                  sha256 = "sha256-fmLcHhD2Cb45OEmIQi8mp9Q1uid1Osy9/kFxelHp70Y=";
+                };
 
-              phases = "installPhase";
+                phases = "installPhase";
 
-              installPhase = ''
+                installPhase = ''
                             mkdir -p $out
                             cp ${src} $out/${file}
                             '';
-            };
-          }
-        ];
-        initExtraBeforeCompInit = ''
+              };
+            }
+          ];
+          initExtraBeforeCompInit = ''
         if [[ $INSIDE_EMACS != vterm && $TERM_PROGRAM != vscode ]]; then
             echo >&2 "Homebrew completion path..."
             if [ -f ${brewpath}/bin/brew ]; then
@@ -2351,7 +2352,7 @@ kitty --listen-on unix:/tmp/mykitty --single-instance --directory "$DIR"
             fi
         fi
     '';
-        initExtra = ''
+          initExtra = ''
         PROMPT=' %{$fg_bold[blue]%}$(get_pwd)%{$reset_color%} ''${prompt_suffix}'
         local prompt_suffix="%(?:%{$fg_bold[green]%}❯ :%{$fg_bold[red]%}❯%{$reset_color%} "
         function get_pwd(){
@@ -2414,6 +2415,12 @@ kitty --listen-on unix:/tmp/mykitty --single-instance --directory "$DIR"
             load_nvmrc
         fi
         '';
+        };
+        direnv = {
+          enable = true;
+          enableZshIntegration = true; # see note on other shells below
+          nix-direnv.enable = true;
+        };
       };
 
       programs.fzf.enable = true;
