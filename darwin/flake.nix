1ace839b4d2da4ad0cf21cb3d8764b18a09766ea
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-03-26 15:37:37 +0900

    Migrate to nixd

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -2,19 +2,25 @@
   description = "Hyunggyu's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:HyunggyuJang/nixpkgs/c95e47775fd13f2ec10014b746a53343c2f0b25f";
-    nix-darwin.url = "github:LnL7/nix-darwin/bcc8afd06e237df060c85bad6af7128e05fd61a3";
+    nixpkgs.url =
+      "github:HyunggyuJang/nixpkgs/c726225724e681b3626acc941c6f95d2b0602087";
+    nix-darwin.url =
+      "github:HyunggyuJang/nix-darwin/83a9a41f1bc62ffedd147df4584501b6bdb992b0";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/1c2c5e4cabba4c43504ef0f8cc3f3dfa284e2dbb";
+    home-manager.url =
+      "github:nix-community/home-manager/1c2c5e4cabba4c43504ef0f8cc3f3dfa284e2dbb";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR/8ef4be789b09122a98de08ef3496c65511b3b35d";
+    nur.url =
+      "github:nix-community/NUR/8ef4be789b09122a98de08ef3496c65511b3b35d";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nur }: {
+  outputs = inputs@{ nix-darwin, nixpkgs, nur, ... }: {
     formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt;
-    darwinConfigurations."Hyunggyus-MacBook-Air" = nix-darwin.lib.darwinSystem {
+    darwinConfigurations.Hyunggyus-MacBook-Air = nix-darwin.lib.darwinSystem {
       modules = [ ./configuration.nix nur.nixosModules.nur ];
       specialArgs = { inherit inputs; };
     };
+    devShells.aarch64-darwin.default =
+      nixpkgs.legacyPackages.aarch64-darwin.mkShell { };
   };
 }
