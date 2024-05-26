ecd189c3bcce8b490d2084db57fd8e3611aea695
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-05-26 10:43:17 +0900

    Migrate to Sonoma & nix from brew

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -2,26 +2,18 @@ self: pkgs:
 
 {
   emacsMacport = pkgs.emacsMacport.overrideAttrs (oldAttrs: {
+    multiTtyPatch = self.fetchurl {
+      url = "https://raw.githubusercontent.com/railwaycat/homebrew-emacsmacport/7abbd0c8887876cb54675afb5545b76fe83c2d31/patches/emacs-mac-29-multi-tty.diff";
+      sha256 = "sha256-RBLONWiePK+Oix11G/NkG0c8067xGInT7NaCR0vyBLA=";
+    };
     notitleBarPatch = self.fetchurl {
-      url = "https://raw.githubusercontent.com/railwaycat/homebrew-emacsmacport/667f0efc08506facfc6963ac1fd1d5b9b777e094/patches/emacs-26.2-rc1-mac-7.5-no-title-bar.patch";
+      url = "https://raw.githubusercontent.com/railwaycat/homebrew-emacsmacport/7abbd0c8887876cb54675afb5545b76fe83c2d31/patches/emacs-26.2-rc1-mac-7.5-no-title-bar.patch";
       sha256 = "8319fd9568037c170f5990f608fb5bd82cd27346d1d605a83ac47d5a82da6066";
     };
-    installTargets = [ "tags" "install" ];
     postPatch = oldAttrs.postPatch + ''
          patch -p1 < $notitleBarPatch
+         patch -p1 < $multiTtyPatch
          '';
-    postInstall = oldAttrs.postInstall + ''
-         for srcdir in src lisp lwlib ; do
-           dstdir=$out/share/emacs/${oldAttrs.version}/$srcdir
-           mkdir -p $dstdir
-           find $srcdir -name "*.[chm]" -exec cp {} $dstdir \;
-           cp $srcdir/TAGS $dstdir
-           echo '((nil . ((tags-file-name . "TAGS"))))' > $dstdir/.dir-locals.el
-         done
-         '';
-    passthru = {
-      pkgs = with pkgs; dontRecurseIntoAttrs (emacsPackagesFor emacsMacport);
-    };
   });
   emacs = self.emacsMacport;
 }
