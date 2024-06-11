1bf594efb50bfff5cde74cc7eaf9028790cec6af
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-06-11 09:58:30 +0900

    Add .direnvrc

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -57,6 +57,24 @@ with lib; rec {
       userconfig = { config, ... }: {
         home.stateVersion = "24.11";
         home.file = {
+          ".direnvrc".text = ''
+            export_alias() {
+              local name=$1
+              shift
+              local alias_dir=$PWD/.direnv/aliases
+              local target="$alias_dir/$name"
+              local oldpath="$PATH"
+              mkdir -p "$alias_dir"
+              if ! [[ ":$PATH:" == *":$alias_dir:"* ]]; then
+                PATH_add "$alias_dir"
+              fi
+
+              echo "#!/usr/bin/env bash" > "$target"
+              echo "PATH=$oldpath" >> "$target"
+              echo "$@" >> "$target"
+              chmod +x "$target"
+            }
+          '';
           ".cargo/bin/rust-analyzer".source = config.lib.file.mkOutOfStoreSymlink
             "${hgj_home}/.rustup/toolchains/stable-aarch64-apple-darwin/bin/rust-analyzer";
           ".gnupg/gpg-agent.conf".text = ''
