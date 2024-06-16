e80aa27b4007b07837b4949ff9a6ae4c0c562310
Hyunggyu Jang <murasakipurplez5@gmail.com>
2024-06-16 19:01:45 +0900

    Fix shell scripts

--- overlays/vscode/patch-vscode.sh
+++ overlays/vscode/patch-vscode.sh
@@ -1,3 +1,5 @@
+set -e
+
 # Define variables from arguments
 extensionPath="$1"
 installationPath="$2"
@@ -21,8 +23,7 @@ browserEntrypointPath="${patchPath}/${browserMain}"
 
 patchBootstrap() {
   local bootstrapResourcesPath="${extensionPath}/resources/${bootstrapName}"
-  local inject='
-  if (entrypoint === "vs/code/electron-main/main") {
+  local inject='  if (entrypoint === "vs/code/electron-main/main") {
     const fs = nodeRequire("fs");
     const p = nodeRequire("path");
     const readFile = fs.readFile;
@@ -35,12 +36,14 @@ patchBootstrap() {
       } else readFile(...arguments);
     };
   }
-  performance.mark("code/fork/willLoadCode");'
+  performance.mark("code/fork/willLoadCode");
+  '
 
   local patchedbootstrapJs
-  patchedbootstrapJs=$(sed "/performance.mark('code\/fork\/willLoadCode');/r /dev/stdin" <<< "$inject" < "${bootstrapResourcesPath}")
-
-  echo "$patchedbootstrapJs" > "${bootstrapPath}"
+  sed "/performance.mark('code\/fork\/willLoadCode');/{
+  r /dev/stdin
+  d
+  }" "${bootstrapResourcesPath}" > "${bootstrapPath}" <<< "$inject"
 }
 
 patchMain() {
@@ -51,7 +54,8 @@ patchMain() {
   local moduleName='vs/modules'
   local patchModule='vs/patch'
 
-  local files='["'"${patchModule}/process.main"', '"${moduleName}/patch.main"', '"${moduleName}/utils"']'
+  local files='["'"${patchModule}/process.main"'", "'"${moduleName}/patch.main"'", "'"${moduleName}/utils"'"]'
+
   local data='define('"${files}"', () => { });'
 
   local patchedMainJs
