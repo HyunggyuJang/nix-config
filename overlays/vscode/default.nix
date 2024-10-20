a964a77622d1c9acc931313aeb95dc4ea6b0d1e2
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2024-10-20 22:41:44 +0900

    Replace all to reflect frameless vscode

--- overlays/vscode/default.nix
+++ overlays/vscode/default.nix
@@ -4,7 +4,7 @@ self: pkgs:
   vscode = pkgs.vscode.overrideAttrs (oldAttrs: {
     postPatch = oldAttrs.postPatch or "" + ''
       FILE="Contents/Resources/app/out/vs/code/electron-main/main.js"
-      sed -i "s/webPreferences/frame:!1,webPreferences/" "$FILE"
+      sed -i "s/webPreferences/frame:!1,webPreferences/g" "$FILE"
     '';
   });
 }
