2c5a91591f8b9120620a912f0941ef4f7a2bfc49
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2025-02-24 14:32:00 +0900

    fix(cargo): SHA256 hash

--- overlays/rust/default.nix
+++ overlays/rust/default.nix
@@ -12,7 +12,7 @@ with prev;
     };
     # target = "x86_64-apple-darwin";
 
-    cargoSha256 = "sha256-r5PWeRvUyzuV26Eq4FWW67WCXBqQmpT8gBJujCys4k4=";
+    cargoSha256 = "sha256-f9jW6yPDnVbpqY5QE/jH6folz9fHfRRwKF70/UwmgFs=";
 
     buildInputs = [ darwin.apple_sdk.frameworks.Security ];
 
