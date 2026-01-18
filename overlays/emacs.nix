88b30347d1e30850e1d0314e63ccdd0a6a751190
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2026-01-18 11:53:51 +0900

    chore: update dependencies

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -1,7 +1,7 @@
 self: pkgs:
 
 {
-  emacsMacport = pkgs.emacsMacport.overrideAttrs (oldAttrs: {
+  emacs-macport = pkgs.emacs-macport.overrideAttrs (oldAttrs: {
     multiTtyPatch = self.fetchurl {
       url = "https://raw.githubusercontent.com/railwaycat/homebrew-emacsmacport/bfd525788e24f2556d1e241156917f1e642f2e29/patches/emacs-mac-29.2-rc-1-multi-tty.diff";
       sha256 = "sha256-Tt5pjI+PVQnjq/Tmqcc+HcOQmw9S9SrUwzBov67T0eQ=";
@@ -15,5 +15,5 @@ self: pkgs:
          patch -p1 < $multiTtyPatch
          '';
   });
-  emacs = self.emacsMacport;
+  emacs = self.emacs-macport;
 }
