fa4504c56816455f370b168f7612ca8f91502b38
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2026-01-28 17:56:56 +0900

    refactor(home-manager): extract targets

--- darwin/home-manager/targets.nix
+++ darwin/home-manager/targets.nix
@@ -1,3 +1,42 @@
 { ... }:
 {
+  targets.darwin.keybindings = {
+    # Control shortcuts
+    "^l" = "centerSelectionInVisibleArea:";
+    "^/" = "undo:";
+    "^_" = "undo:";
+    "^ " = "setMark:";
+    "^w" = "deleteToMark:";
+    "^u" = "deleteToBeginningOfLine:";
+    "^g" = "_cancelKey:";
+    # Meta shortcuts
+    "~y" = "yankPop:";
+    "~f" = "moveWordForward:";
+    "~b" = "moveWordBackward:";
+    "~p" = "selectPreviousKeyView:";
+    "~n" = "selectNextKeyView:";
+    # Excaping XML expressions should be done automatically!
+    "~&lt;" = "moveToBeginningOfDocument:";
+    "~&gt;" = "moveToEndOfDocument:";
+    "~v" = "pageUp:";
+    "~/" = "complete:";
+    "~c" = [ "capitalizeWord:" "moveForward:" "moveForward:" ];
+    "~u" = [ "uppercaseWord:" "moveForward:" "moveForward:" ];
+    "~l" = [ "lowercaseWord:" "moveForward:" "moveForward:" ];
+    "~d" = "deleteWordForward:";
+    "^~h" = "deleteWordBackward:";
+    "~t" = "transposeWords:";
+    "~\\@" = [ "setMark:" "moveWordForward:" "swapWithMark:" ];
+    "~h" = [ "setMark:" "moveToEndOfParagraph:" "swapWithMark:" ];
+    # C-x shortcuts
+    "^x" = {
+      "u" = "undo:";
+      "k" = "performClose:";
+      "^f" = "openDocument:";
+      "^x" = "swapWithMark:";
+      "^m" = "selectToMark:";
+      "^s" = "saveDocument:";
+      "^w" = "saveDocumentAs:";
+    };
+  };
 }
