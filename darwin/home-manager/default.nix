7b22d50443057e1f77a7d0cb46602b3e0d1b9f05
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2026-01-28 17:47:12 +0900

    refactor(home-manager): add default module

---
+++ darwin/home-manager/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, inputs, owner, hostName, machineType, hgj_home, hgj_projects, hgj_darwin_home, hgj_localbin, brewpath, ... }:
+{
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = false;
+  home-manager.extraSpecialArgs = {
+    inherit hostName owner machineType hgj_home hgj_projects hgj_darwin_home hgj_localbin brewpath inputs;
+  };
+  home-manager.users.${owner} = {
+    home.stateVersion = "24.11";
+    imports = [
+      ./files.nix
+      ./targets.nix
+      ./xdg.nix
+      ./programs/vscode.nix
+      ./programs/zsh.nix
+      ./programs/direnv.nix
+      ./programs/emacs.nix
+      ./programs/firefox.nix
+      ./programs/fzf.nix
+      ./programs/browserpass.nix
+    ];
+  };
+}
