1d7c39ec16a65ea38b857f8b39ae4d7515b41d9c
Hyunggyu Jang <murasakipurplez5@gmail.com>
2026-02-01 16:53:11 +0900

    Enable home-manager backups

--- darwin/home-manager/default.nix
+++ darwin/home-manager/default.nix
@@ -16,6 +16,7 @@
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = false;
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.extraSpecialArgs = {
     inherit
       hostName
