c67e04b84a20774861b8870d6b092b7f12e4929b
Hyunggyu Jang <murasakipurplez5@gmail.com>
2026-02-08 10:12:55 +0900

    darwin: track Doom Emacs bootstrap and DOOMDIR link

--- darwin/home-manager/programs/emacs.nix
+++ darwin/home-manager/programs/emacs.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ lib, pkgs, hgj_home, ... }:
+let
+  emacsDir = "${hgj_home}/.emacs.d";
+  doomRepo = "git@github.com:HyunggyuJang/doom-emacs.git";
+in
 {
   programs = {
     emacs = {
@@ -7,4 +11,12 @@
       extraPackages = (epkgs: [ epkgs.vterm ]);
     };
   };
+
+  home.activation.bootstrapDoomEmacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -e "${emacsDir}" ]; then
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone "${doomRepo}" "${emacsDir}"
+    elif [ ! -d "${emacsDir}/.git" ]; then
+      echo "warning: ${emacsDir} exists but is not a git repository; skipping bootstrap" >&2
+    fi
+  '';
 }
